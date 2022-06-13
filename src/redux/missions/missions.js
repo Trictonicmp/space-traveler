afe56793fd39881f51ac972154870351a2705634
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-13 22:08:39 +0000

    Fix linter errors

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,17 +1,17 @@
 // ACTIONS
 
-//ACTION CREATORS
+// ACTION CREATORS
 
 const initialState = [];
 
 const reducer = (state = initialState, action) => {
-    switch (action) {
-        case 'FETCH_MISSON':
-            return [...state, action.payload];
-        
-        default:
-            return state;
-   }
+  switch (action) {
+    case 'FETCH_MISSON':
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
