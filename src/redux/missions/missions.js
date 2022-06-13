18cd5facbd5e10b4c582bcf485ffccb93bc98853
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-13 22:01:58 +0000

    Add action to mission reducer

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,8 +2,16 @@
 
 //ACTION CREATORS
 
-const reducer = () => {
+const initialState = [];
 
+const reducer = (state = initialState, action) => {
+    switch (action) {
+        case 'FETCH_MISSON':
+            return [...state, action.payload];
+        
+        default:
+            return state;
+   }
 };
 
 export default reducer;
