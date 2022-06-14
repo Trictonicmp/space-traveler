ee3b3132e4189b8ca01d0456beea165c669e827e
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-14 18:22:38 +0000

    Add profile page route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from './components/NavBar/Nav';
 import Mission from './components/pages/Mission';
 import Rocket from './components/pages/Rocket';
+import Profile from './components/pages/Profile';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Rocket />} />
         <Route path="/mission" element={<Mission />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
