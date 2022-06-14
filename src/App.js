228996a38960561b976ae3fa0402a052aa941bda
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-14 17:49:22 +0000

    Set page routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/NavBar/Nav';
+import Mission from './components/pages/Mission';
+import Rocket from './components/pages/Rocket';
+
 function App() {
   return (
-    <div className="App" />
+    <>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Rocket />} />
+        <Route path='/mission' element={<Mission />} />
+      </Routes>
+    </>
   );
 }
 
