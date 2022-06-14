8cb69172b1c629a476e817953af6e87037d7f562
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-14 17:48:28 +0000

    Fix linter errors

--- src/components/NavBar/NavLinks.js
+++ src/components/NavBar/NavLinks.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function NavLinks() {
-    return (
-        <ul>
-            <li>Rockets</li>
-            <li>Missions</li>
-        </ul>
-    );
+  return (
+    <ul>
+      <li>Rockets</li>
+      <li>Missions</li>
+    </ul>
+  );
 }
 
 export default NavLinks;
--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Mission() {
-    return (
-        <div>
-            <h1>Mission Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Mission Page</h1>
+    </div>
+  );
 }
 
 export default Mission;
