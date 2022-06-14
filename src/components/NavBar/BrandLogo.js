5c52f329fd7241255f9dbb0e34c51b596cf31491
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-14 18:27:10 +0000

    Resize logo image

--- src/components/NavBar/BrandLogo.js
+++ src/components/NavBar/BrandLogo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import LogoImage from '../../assets/images/logo.png';
 
 function BrandLogo() {
-  return <img src={LogoImage} alt="Logo" />;
+  return <img src={LogoImage} alt="Logo" width="70px" />;
 }
 
 export default BrandLogo;
