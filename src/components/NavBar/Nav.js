d727583dd9c57dcb2149abd50bd19eaf48d6b6ec
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-14 17:28:36 +0000

    Import logo and navlinks into nav components

---
+++ src/components/NavBar/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
+
+function Nav() {
+  return (
+    <nav>
+      <Logo />
+      <NavLinks />
+    </nav>
+  );
+}
+
+export default Nav;
