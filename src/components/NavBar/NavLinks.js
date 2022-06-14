6419dec1873fa105b5c71b543a276328262b17e3
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-14 17:22:17 +0000

    Create navlinks component

---
+++ src/components/NavBar/NavLinks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NavLinks() {
+    return (
+        <ul>
+            <li>Rockets</li>
+            <li>Missions</li>
+        </ul>
+    );
+}
+
+export default NavLinks;
