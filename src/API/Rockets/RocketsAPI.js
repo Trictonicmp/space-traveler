7c3fee8d81518dc3af1533b2c6a98b8f65a05805
Trictonicmp <code_shadow@hotmail.com>
2022-06-14 15:40:05 -0500

    Add Rocket component

--- src/API/Rockets/RocketsAPI.js
+++ src/API/Rockets/RocketsAPI.js
@@ -1,15 +1,24 @@
 import { ROCKETS_ENDPOINT } from '../APIconfig';
 
 const getRocketsFromAPI = async (errorCallback = null) => {
-  let data = [];
+  let rockets = [];
   try {
     const response = await fetch(ROCKETS_ENDPOINT, { method: 'GET' });
-    data = await response.json();
+    const rocketsFromAPI = await response.json();
+    rockets = rocketsFromAPI.map((rocket) => {
+      const newRocket = {
+        id: rocket.id,
+        rocketName: rocket.rocket_name,
+        description: rocket.description,
+        imageURL: rocket.flickr_images[0],
+      };
+      return newRocket;
+    });
   } catch (error) {
     if (errorCallback) errorCallback();
   }
 
-  return data;
+  return rockets;
 };
 
 export default getRocketsFromAPI;
--- src/components/pages/Rockets.jsx
+++ src/components/pages/Rockets.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { getRockets } from '../../redux/rockets/Rockets';
+import Rocket from '../Rocket';
 
 import styles from '../../css/components/RocketsContainer.module.css';
 
@@ -10,12 +11,20 @@ const Rockets = () => {
 
   useEffect(() => {
     dispatch(getRockets());
-    console.log(state);
   }, [dispatch]);
 
+  const rocketComponents = state.map((rocket) => (
+    <Rocket
+      id={rocket.id}
+      key={rocket.id}
+      rocketName={rocket.rocketName}
+      description={rocket.description}
+      imageURL={rocket.imageURL}
+    />
+  ));
   return (
     <ul className={styles.rocketsContainer}>
-      1
+      { rocketComponents }
     </ul>
   );
 };
