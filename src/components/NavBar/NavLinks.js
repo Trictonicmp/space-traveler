8dec07923330aa28f5718454ad57d3423f0abb86
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-14 18:36:42 +0000

    Set class name to list

--- src/components/NavBar/NavLinks.js
+++ src/components/NavBar/NavLinks.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../css/components/nav.css';
 
 function NavLinks() {
   return (
-    <ul>
+    <ul className="nav-links">
       <Link to="/">Rockets</Link>
       <Link to="/mission">Missions</Link>
       <Link to="/profile">Profile</Link>
