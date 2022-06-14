463b6c3d13fc6bc07b4674f42cec27339f0d8eda
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-14 18:36:21 +0000

    Add logo text to image

--- src/components/NavBar/BrandLogo.js
+++ src/components/NavBar/BrandLogo.js
@@ -2,7 +2,12 @@ import React from 'react';
 import LogoImage from '../../assets/images/logo.png';
 
 function BrandLogo() {
-  return <img src={LogoImage} alt="Logo" width="70px" />;
+  return (
+    <div className="d-flex">
+      <img src={LogoImage} alt="Logo" width="70px" />
+      <h2>Space Travelers' Hub</h2>
+    </div>
+  )
 }
 
 export default BrandLogo;
