befa25426d6f898d780742a095ba7b30bf07583e
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-15 16:45:29 +0000

    Create action and action creators

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,15 +1,15 @@
+import fetchMissions from '../../API/Missions/MissionsAPI';
 // ACTIONS
-const FETCH = 'FETCH_MISSION'
+const FETCH_MISSION = 'FETCH_MISSION';
 
+const FETCH = (missons) => ({ type: FETCH_MISSION, payload: missons });
 
 // ACTION CREATORS
-const FETCH_MISSION = () => async (dispatch) => {
-  try {
-    
-  } catch (error) {
-    
-  }
-}
+export const fetchThunk = () => async (dispatch) => {
+  const missions = await fetchMissions();
+  console.log(missions);
+  dispatch(FETCH(missions));
+};
 
 const initialState = [];
 
