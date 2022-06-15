61b5b1be7616186a8dbdf62ddfff16727c6a955e
Trictonicmp <code_shadow@hotmail.com>
2022-06-15 15:35:29 -0500

    Add basic profile structure

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import styles from '../../css/components/ProfileContainer.module.css';
 
 function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <section className={styles.profileContainer}>
+      <div>
+        <h2>My Missions</h2>
+        <ul>
+          <li>one</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>My Rockets</h2>
+        <ul>
+          <li>two</li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
