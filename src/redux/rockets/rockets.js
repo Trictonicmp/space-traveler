6200b9de48186ead38c46c21f4c67a306c418122
Trictonicmp <code_shadow@hotmail.com>
2022-06-15 12:28:16 -0500

    Added Reserve rocket action, Added reserveRockets action creator and Added reducer for reserve rocket action

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -2,8 +2,14 @@ import getRocketsFromAPI from '../../API/Rockets/RocketsAPI';
 
 // ACTIONS
 export const GET_ROCKETS = 'space-traveler/rockets/GET_ROCKETS';
+export const RESERVE_ROCKET = 'space-traveler/rockets/RESERVE_ROCKET';
 
 // ACTION CREATORS
+export const reserveRocket = (id) => ({
+  type: RESERVE_ROCKET,
+  payload: id,
+});
+
 export const getRockets = () => async (dispatch) => {
   const rockets = await getRocketsFromAPI();
 
@@ -17,6 +23,11 @@ const reducer = (state = [], action) => {
   switch (action.type) {
     case GET_ROCKETS:
       return action.payload;
+    case RESERVE_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.payload) return rocket;
+        return { ...rocket, reserved: true };
+      });
     default:
       return state;
   }
