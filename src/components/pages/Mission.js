46ddbff2f96235479384412c1cf08d44e7cde96b
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-15 17:25:50 +0000

    Import single mission component

--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchThunk } from '../../redux/missions/missions';
+import SingleMission from '../Mission/SingleMission';
 
 function Mission() {
   const dispatch = useDispatch();
+  // const selector = useSelector();
+
+  // const missions = selector((state) => state.missions);
 
   useEffect(() => {
     dispatch(fetchThunk());
   }, []);
 
   return (
-    <div>
-      <h1>Mission Page</h1>
-    </div>
+    <SingleMission />
   );
 }
 
