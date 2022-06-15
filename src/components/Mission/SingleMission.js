37f18ba3dee10f40eeb9cfefacd9b84de92466d9
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-15 17:51:49 +0000

    Create table header component

--- src/components/Mission/SingleMission.js
+++ src/components/Mission/SingleMission.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../../css/components/mission.css';
 
+export function TableHeader() {
+  return (
+    <header className="header">
+      <div>Mission</div>
+      <div className="description">Description</div>
+      <div>Status</div>
+      <div>Button</div>
+    </header>
+  );
+}
+
 function SingleMission() {
   return (
     <div>
-      <h1>Mission</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
