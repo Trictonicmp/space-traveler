49ed1795b681ad5047bf0d86a31a17e28b110926
Trictonicmp <code_shadow@hotmail.com>
2022-06-15 15:06:03 -0500

    Add dispatch to load rockets from APP.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getRockets } from './redux/rockets/rockets';
 import Nav from './components/NavBar/Nav';
 import Mission from './components/pages/Mission';
 import Rockets from './components/pages/Rockets';
 import Profile from './components/pages/Profile';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
   return (
     <>
       <Nav />
