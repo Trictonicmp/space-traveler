50ae5ec9e07571e93d300924f7f694b2903a8cb9
Trictonicmp <code_shadow@hotmail.com>
2022-06-15 15:09:57 -0500

    Remove useEffect in RocketsContainer to not reload the state

--- src/components/pages/Rockets.jsx
+++ src/components/pages/Rockets.jsx
@@ -1,17 +1,10 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux/es/exports';
-import { getRockets } from '../../redux/rockets/rockets';
+import { useSelector } from 'react-redux/es/exports';
 import Rocket from '../Rocket';
 
 import styles from '../../css/components/RocketsContainer.module.css';
 
 const Rockets = () => {
   const state = useSelector((state) => state.rockets);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getRockets());
-  }, []);
 
   const rocketComponents = state.map((rocket) => (
     <Rocket rocket={rocket} key={rocket.id} />
