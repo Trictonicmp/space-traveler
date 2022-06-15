6ac1ef5bbd4b0e9bd583f746b18148508173a01a
Trictonicmp <code_shadow@hotmail.com>
2022-06-15 16:08:17 -0500

    Add functionality to display rockets and missions that are reserved

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,19 +1,46 @@
+import { useSelector } from 'react-redux';
 import styles from '../../css/components/ProfileContainer.module.css';
 
 function Profile() {
+  const missionsState = useSelector((state) => state.missions);
+  const rocketsState = useSelector((state) => state.rockets);
+
+  const reservedMissions = missionsState.map((mission) => {
+    if (mission.reserved) {
+      return (
+        <li key={mission.id}>
+          { mission.mission }
+        </li>
+      );
+    }
+    return '';
+  });
+
+  const reservedRockets = rocketsState.map((rocket) => {
+    if (rocket.reserved) {
+      return (
+        <li key={rocket.id}>
+          { rocket.rocketName }
+        </li>
+      );
+    }
+
+    return '';
+  });
+
   return (
     <section className={styles.profileContainer}>
       <div>
         <h2>My Missions</h2>
         <ul>
-          <li>one</li>
+          { reservedMissions }
         </ul>
       </div>
 
       <div>
         <h2>My Rockets</h2>
         <ul>
-          <li>two</li>
+          { reservedRockets }
         </ul>
       </div>
     </section>
