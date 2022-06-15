f8fc0f7647f8ead57ee153ff4b0980cf5a548a61
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-15 16:45:55 +0000

    Dispatch thunk on page load

--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchThunk } from '../../redux/missions/missions';
 
 function Mission() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchThunk());
+  }, []);
+
   return (
     <div>
       <h1>Mission Page</h1>
