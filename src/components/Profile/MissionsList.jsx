eb458071980a1cb70bd9f7260b2b1cdb90c3f821
Trictonicmp <code_shadow@hotmail.com>
2022-06-16 10:17:32 -0500

    Refactor Profile to have 2 list components instead of having the whole functionallity in profile

---
+++ src/components/Profile/MissionsList.jsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { toggleReserveRocket } from '../../redux/rockets/rockets';
+import styles from '../../css/components/ProfileContainer.module.css';
+import buttonStyle from '../../css/components/RocketsButton.module.css';
+
+const MissionsList = (props) => {
+  const { missions } = props;
+  const dispatch = useDispatch();
+
+  const reservedMissionsJSX = missions.map((mission) => (
+    <li key={mission.id} className={styles.profileListItem}>
+      { mission.mission }
+      <button
+        type="button"
+        className={`
+          ${buttonStyle.button}
+          ${buttonStyle.outline}
+        `}
+        onClick={() => dispatch(toggleReserveRocket(mission.id))}
+      >
+        Leave Mission
+      </button>
+    </li>
+  ));
+
+  if (missions.length === 0) {
+    reservedMissionsJSX.push(
+      <li key={0} className={styles.profileListNoItems}>
+        No Missions Joined
+      </li>,
+    );
+  }
+
+  return (
+    <>
+      { reservedMissionsJSX }
+    </>
+  );
+};
+
+MissionsList.propTypes = {
+  missions: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    mission: PropTypes.string,
+    description: PropTypes.string,
+  })).isRequired,
+};
+
+export default MissionsList;
--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,67 +1,29 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { toggleReserveRocket } from '../../redux/rockets/rockets';
+import { useSelector } from 'react-redux';
 import styles from '../../css/components/ProfileContainer.module.css';
-import buttonStyle from '../../css/components/RocketsButton.module.css';
+import RocketsList from '../Profile/RocketsList';
+import MissionsList from '../Profile/MissionsList';
+// import buttonStyle from '../../css/components/RocketsButton.module.css';
 
 function Profile() {
   const missionsState = useSelector((state) => state.missions);
   const rocketsState = useSelector((state) => state.rockets);
-  const dispatch = useDispatch();
 
   const reservedMissions = missionsState.filter((mission) => mission.reserved);
   const reservedRockets = rocketsState.filter((rocket) => rocket.reserved);
-
-  const reservedMissionsJSX = missionsState.map((mission) => (
-    <li key={mission.id} className={styles.profileListItem}>
-      { mission.mission }
-    </li>
-  ));
-
-  if (reservedMissions.length === 0) {
-    reservedMissionsJSX.push(
-      <li key={0} className={styles.profileListNoItems}>
-        No Missions Joined
-      </li>,
-    );
-  }
-
-  const reservedRocketsJSX = reservedRockets.map((rocket) => (
-    <li key={rocket.id} className={styles.profileListItem}>
-      { rocket.rocketName }
-      <button
-        type="button"
-        className={`
-          ${buttonStyle.button}
-          ${buttonStyle.outline}
-        `}
-        onClick={() => dispatch(toggleReserveRocket(rocket.id))}
-      >
-        Cancel Reservation
-      </button>
-    </li>
-  ));
-
-  if (reservedRockets.length === 0) {
-    reservedRocketsJSX.push(
-      <li key={0} className={styles.profileListNoItems}>
-        No Rockets Reserved
-      </li>,
-    );
-  }
 
   return (
     <section className={styles.profileContainer}>
       <div className={styles.profileListSection}>
         <h2>My Missions</h2>
         <ul className={styles.profileListItemsContainer}>
-          { reservedMissionsJSX }
+          <MissionsList missions={reservedMissions} />
         </ul>
       </div>
 
       <div className={styles.profileListSection}>
         <h2>My Rockets</h2>
         <ul className={styles.profileListItemsContainer}>
-          { reservedRocketsJSX }
+          <RocketsList rockets={reservedRockets} />
         </ul>
       </div>
     </section>
